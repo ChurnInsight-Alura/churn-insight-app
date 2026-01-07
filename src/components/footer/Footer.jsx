474a1f20b5da430ccc23c145d605a03a210547bf
Luis <luis23797@gmail.com>
2026-01-06 21:24:14 -0600

    Definicion de estructura general y variables

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <footer className="p-3 border-t border-primary/10 bg-gray-50/30">
+      <h1 className="text-sm text-primary/50 font-medium">© 2026 Mi Panel</h1>
+    </footer>
+  )
+}
